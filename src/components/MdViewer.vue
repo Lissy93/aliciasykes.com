d7c00d33580a473370cb3b35ad58352f9b71f695
Alicia Sykes <sykes.alicia@gmail.com>
2019-02-06 22:25:54 +0000

    Built out the structure for the legal page

---
+++ src/components/MdViewer.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="md-viewer">
+    <vue-markdown class="md-content"><slot></slot></vue-markdown>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import VueMarkdown from 'vue-markdown';
+@Component({
+    components: {
+        VueMarkdown,
+    },
+})
+export default class MdViewer extends Vue {
+  @Prop() private pathToMdFile!: string;
+}
+</script>
+
+<style lang="scss">
+  .md-content{
+    h1, h2, h3, h4, h5 {
+      text-align: left;
+    }
+    p, a, ul, li, span {
+      text-align: left;
+      font-size: 1rem;
+    }
+    h2 {
+      font-size: 2em;
+    }
+    h3 {
+      font-size: 1.2rem;
+      font-weight: bold;
+    }
+    h4, h5{
+      font-size: 1rem;
+      font-weight: bold;
+    }
+  }
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-    baseUrl: "/"
+    baseUrl: "/",
+    configureWebpack: {
+        module: {
+            rules: [
+                {
+                    test: /\.md$/,
+                    use: [
+                        {
+                            loader: "html-loader"
+                        },
+                        {
+                            loader: "markdown-loader",
+                            options: {}
+                        }
+                    ]
+              }
+            ]
+        }
+    }
 }
