ddf7ccb75588666a8379eb0dd8feca581ac7d6dd
Alicia Sykes <alicia@omg.lol>
2022-02-20 15:06:11 +0000

    :coffin: Removes analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import App from './App.vue';
-import VueAnalytics from 'vue-analytics';
 import router from './router';
 import VueHead from 'vue-head';
 import './registerServiceWorker';
@@ -8,11 +7,6 @@ import './registerServiceWorker';
 Vue.config.productionTip = false;
 
 Vue.use(VueHead);
-
-Vue.use(VueAnalytics, {
-  id: 'UA-53932003-1',
-  router,
-});
 
 new Vue({
   router,
